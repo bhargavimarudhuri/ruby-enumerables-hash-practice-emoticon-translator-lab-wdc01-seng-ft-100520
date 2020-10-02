d8a5e65678f1e2adea16fc21800df852200e53a7
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-10-02 04:53:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,14 +4,14 @@ require "yaml"
 def load_library(file)
   # code goes here
   emoticons = YAML.load_file(file)
-  puts emoticons
-  new_emoticons = emoticons.map do |key, val|
+  new_emoticons = {}
+  emoticons.each do |key, val|
     updated_element = {}
     lang_map = {}
     lang_map[:english] = val[0]
     lang_map[:japanese] = val[1]
     updated_element[key] = lang_map
-    updated_element
+    new_emoticons.push(updated_element)
   end
   new_emoticons
 end
